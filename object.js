d242daccb6b324dc12ed795cbd52eebca5d7bbde
mohsinkkc <mohsinkkc1@gmail.com>
2025-03-06 16:16:14 +0530

    add object.js file in repo

---
+++ object.js
@@ -0,0 +1,57 @@
+const sum = Symbol("welcome")
+
+const details = {
+    name:"mohsin",
+    sum:"Bye Bye",
+    age:21,
+    email:"mohsin@gmail.com",
+    "full name":"mohsin kkc"
+}
+
+console.log(details.email)
+console.log(details["full name"])
+console.log(details.sum)
+
+
+details.greet = function(){
+    console.log("Hi !! Guys")
+}
+
+
+details.greet2 = function(){
+    console.log(`Hey Guys ${this["full name"]} we are here with ${this.name}`)
+}
+console.log(details.greet());
+console.log(details.greet2());
+
+
+
+const user = {}
+
+user.id = "1285bdg"
+user.age = 25
+user.email = "kkc@gmail.com"
+
+// console.log(user)
+
+const regularuser = {
+    name:{
+        fullname:{
+            firstname:"mohsin",
+            lastname:"kkc",
+        }
+    },
+}
+
+console.log(regularuser.name.fullname)
+
+const obj1 ={1:"a" , 2:"b"}
+const obj2 ={3:"a" , 6:"b"}
+const obj3 ={4:"a" , 7:"b"}
+const obj4 ={5:"a" , 8:"b"}
+
+// const obj5 = Object.assign({}, obj1,obj2,obj3,obj4)
+// console.log(obj5)
+
+const obj5 = {...obj1,...obj2,...regularuser}
+console.log(obj5)
