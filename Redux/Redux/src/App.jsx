992f9ad75727fc2704ddc03772159c1f3f3ae434
mohsinkkc <mohsinkkc1@gmail.com>
2025-05-09 20:04:07 +0530

    push

--- Redux/Redux/src/App.jsx
+++ Redux/Redux/src/App.jsx
@@ -1,12 +1,52 @@
 
-
+import { useDispatch, useSelector } from 'react-redux'
+import './App.css'
+import { decrement, increment, incrementByAmount, Reset } from './feature/counterSlice'
+import { useState } from 'react'
 
 function App() {
-  
+  const [amount,setAmount] = useState(0);
+  const count=useSelector((state)=>state.counter.value)
+  const dispatch=useDispatch()
+
+  const handleIncremnetClick=()=>{
+    dispatch(increment())
+  }
+  const handleDecremnetClick=()=>{
+    dispatch(decrement())
+  }
+  const handleResetClick=()=>{
+    dispatch(Reset())
+  }
+  const handleIncrementAmountClick=()=>{
+    dispatch(incrementByAmount(amount))
+  }
 
   return (
     <>
-      <div>hello</div>
+      <div>
+      <div>
+        <button onClick={handleIncremnetClick}>+</button>
+        <p>Count : {count}</p>
+        <button onClick={handleDecremnetClick}>-</button>
+        <br/>
+        <br/>
+        <div>
+        <button onClick={handleResetClick}>Reset</button>
+        </div>
+        <br/>
+        <br/>
+        <input 
+        type='number'
+        placeholder='Enter number'
+        value={amount}
+        onChange={(e)=>setAmount(e.target.value)}
+        />
+        <br />
+        <br />
+        <button onClick={handleIncrementAmountClick}>Increment Amount</button>
+      </div>
+      </div>
     </>
   )
 }
